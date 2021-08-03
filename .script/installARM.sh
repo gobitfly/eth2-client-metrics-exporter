ec8f44730fe8c363e95ddd550a030bc69a70ac84
Manuel <manuel@caspari.at>
2021-08-03 09:11:56 +0200

    download scripts for beaconcha.in

---
+++ .script/installARM.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Downloading beaconcha.in metric exporter..."
+curl -s https://api.github.com/repos/gobitfly/eth2-client-metrics-exporter/releases/latest | grep 'eth2-client-metrics-exporter-linux-arm64' | cut -d : -f 2,3 |  tr -d \" | wget -qi -
+chmod +x eth2-client-metrics-exporter-linux-arm64
+echo "Done."
